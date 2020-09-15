67b35a4a30213d0d3eb15cd22053e5ed582433c3
Timothy Axford <35697045+TAxford@users.noreply.github.com>
2020-09-15 13:21:53 -0400

    Names button to Form 2 and placed code so when hit, switches to form 2.

--- Lab 2/Lab 2/Form1.vb
+++ Lab 2/Lab 2/Form1.vb
@@ -6,4 +6,9 @@
     Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Form2.Show()
+        Me.Hide()
+    End Sub
 End Class

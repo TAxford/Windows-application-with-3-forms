7e9f2421d7ca5b1f05b1bd09303a97cab531c212
Timothy Axford <35697045+TAxford@users.noreply.github.com>
2020-09-15 13:40:36 -0400

    Added code to Background color button on Form 2 to change background color.

--- Lab 2/Lab 2/Form2.vb
+++ Lab 2/Lab 2/Form2.vb
@@ -1,3 +1,22 @@
 ﻿Public Class Form2
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Form1.Show()
+        Me.Hide()
+    End Sub
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Backgroundcolor As New ColorDialog
+
+        If Backgroundcolor.ShowDialog = Windows.Forms.DialogResult.OK Then
+
+            Me.BackColor = Backgroundcolor.Color
+
+        Else
+
+
+
+
+        End If
+
+    End Sub
 End Class
